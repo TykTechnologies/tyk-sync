e76ddaf1fe0b7288a143db79a6a246a7b5f176b8
Tomas Buchaillot <tombuchaillot89@gmail.com>
2022-07-25 10:24:19 +0200

    adding version cmd (#73)
    (cherry picked from commit 437742c1af013a438204cd2061437747608eb279)

---
+++ cmd/version.go
@@ -0,0 +1,23 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+const VERSION = "1.2.2"
+
+func init() {
+	RootCmd.AddCommand(versionCmd)
+}
+
+var versionCmd = &cobra.Command{
+	Use:     "version",
+	Aliases: []string{"v"},
+	Short:   "Tyk-sync version",
+	Long:    `This command will show the current Tyk-Sync version.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("v" + VERSION)
+	},
+}
