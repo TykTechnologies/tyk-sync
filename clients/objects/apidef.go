45acacc50b73f9045d94017b6a4038e8b1d74ea5
tbuchaillot <tombuchaillot89@gmail.com>
2022-12-15 20:06:59 +0100

    adding second backwards compatibility layer around external_oauth

--- clients/objects/apidef.go
+++ clients/objects/apidef.go
@@ -25,4 +25,5 @@ type APIDefinition struct {
 	apidef.APIDefinition
 	Scopes                *apidef.Scopes                `json:"scopes,omitempty"`
 	AnalyticsPluginConfig *apidef.AnalyticsPluginConfig `json:"analytics_plugin,omitempty"`
+	ExternalOAuth         *apidef.ExternalOAuth         `json:"external_oauth"`
 }
