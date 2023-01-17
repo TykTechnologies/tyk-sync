72513b10c50b3c94e307bc0ce320cde19671b26c
Tomas Buchaillot <tombuchaillot89@gmail.com>
2023-01-17 17:52:03 +0100

    fix version conflict

--- cmd/version.go
+++ cmd/version.go
@@ -6,11 +6,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-<<<<<<< HEAD
-const VERSION = "1.2.3"
-=======
 const VERSION = "1.2.4"
->>>>>>> 43a5f0f... Fix/backwards apidef (#95)
 
 func init() {
 	RootCmd.AddCommand(versionCmd)
