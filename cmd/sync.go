95bc07acec98e5375af5b36529e6aa5f3875e456
Komal Sukhani <komaldsukhani@gmail.com>
2024-06-14 16:20:00 +0530

    Remove --template flag from tyk-sync. Mark --apis, --policies flag as deprecated

--- cmd/sync.go
+++ cmd/sync.go
@@ -44,9 +44,16 @@ func init() {
 	syncCmd.Flags().Bool("allow-unsafe-oas", false, "Use Tyk Classic endpoints in Tyk Dashboard API for Tyk OAS APIs (optional)")
 	syncCmd.Flags().StringSlice("policies", []string{}, "Specific Policies ids to sync")
 	syncCmd.Flags().StringSlice("apis", []string{}, "Specific Apis ids to sync")
-	syncCmd.Flags().StringSlice("templates", []string{}, "List of template or Assets IDs to sync (optional")
 
 	if err := syncCmd.Flags().MarkDeprecated("allow-unsafe-oas", "OAS API can be synced without the flag."); err != nil {
 		fmt.Printf("Failed to mark `allow-unsafe-oas` flag as deprecated: %v", err)
+	}
+
+	if err := syncCmd.Flags().MarkDeprecated("apis", "The flag will be removed in upcoming release"); err != nil {
+		fmt.Printf("Failed to mark `apis` flag as deprecated: %v", err)
+	}
+
+	if err := syncCmd.Flags().MarkDeprecated("policies", "The flag will be removed in upcoming release"); err != nil {
+		fmt.Printf("Failed to mark `policies` flag as deprecated: %v", err)
 	}
 }
